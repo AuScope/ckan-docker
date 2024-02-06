56a9a9101e5ebdec0f4adb92ae777cde598ec111
root <root@avre-test-dev.noreply.csiro.au>
2024-02-06 16:11:14 +1100

    Citation is auto-created on dataset display. Turned off ability for user to add non-organizational datasets. Change owner of storage directory in dev startup script.

--- ckan/src/ckanext-auscope-theme/ckanext/auscope_theme/logic/schema.py
+++ ckan/src/ckanext-auscope-theme/ckanext/auscope_theme/logic/schema.py
@@ -1,4 +1,5 @@
 import ckan.plugins.toolkit as tk
+import json
 
 
 def auscope_theme_get_sum():
@@ -9,3 +10,34 @@ def auscope_theme_get_sum():
         "left": [not_empty, convert_int],
         "right": [not_empty, convert_int]
     }
+
+@tk.chained_action
+def after_dataset_show(context, pkg_dict):
+    """
+    Add the Citation details to the pkg_dict so it can be displayed
+    Format:
+        Authors (PublicationYear): Title. Publisher. (ResourceType). Identifier
+    Example:
+        Irino, T; Tada, R (2009): Chemical and mineral compositions of sediments from ODP Site 127-797. V. 2.1. Geological Institute, University of Tokyo. (dataset). https://doi.org/10.1594/PANGAEA.726855
+    """
+    citation = ''
+    author_list = json.loads(pkg_dict['author'])
+    for i in range(0, len(author_list)):
+        citation += author_list[i]['author_name']
+        if i != len(author_list) - 1:
+            citation += ', '
+        else:
+            # May want to reduce to year
+            citation += ' (' + pkg_dict['publication_date'] + '): '
+    citation += pkg_dict['title']
+
+    if citation[len(citation) -1] != '.':
+        citation += '.'
+    citation += ' '
+
+    citation += pkg_dict['publisher'] + ' (' + pkg_dict['resource_type'] +') '
+    if 'doi' in pkg_dict:
+        citation += pkg_dict['doi']
+
+    pkg_dict['citation'] = citation
+
