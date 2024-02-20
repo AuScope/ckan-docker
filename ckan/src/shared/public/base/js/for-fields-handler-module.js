bff91591793549883fa92e66062eac1aca782294
Neda Taherifar <n.taherifar@gmail.com>
2024-02-20 14:20:24 +0800

    Fix a bug on Fields of Research

--- ckan/src/shared/public/base/js/for-fields-handler-module.js
+++ ckan/src/shared/public/base/js/for-fields-handler-module.js
@@ -1,36 +1,50 @@
 this.ckan.module('for-fields-handler-module', function ($, _) {
   return {
     initialize: function () {
-      this.selectElement = $('#field-fields_of_reserch'); 
+      this.selectElement = $('#field-fields_of_research');
       this.codeElement = $('#field-fields_of_research_code');
       this.fetchAndPopulateTerms();
       this.selectElement.change(this.updateCodesField.bind(this));
     },
+
     fetchAndPopulateTerms: function () {
       var self = this;
-      // Use the proxy endpoint
       var proxyUrl = '/api/proxy/fetch_terms';
-
       $.ajax({
         url: proxyUrl,
         method: 'GET',
         success: function (data) {
           self.populateDropdown(data.result.items);
-          
+
         },
         error: function (xhr, status, error) {
           console.error('Error fetching FoR terms via proxy:', error);
         }
       });
     },
-
     populateDropdown: function (items) {
       var self = this;
+      var selectedValues = this.options.selectedValues;
+      if (typeof selectedValues === 'string') {
+        selectedValues = selectedValues.replace(/^\{|\}$/g, '').split(',');
+      } else if (typeof selectedValues === 'number') {
+        selectedValues = [selectedValues.toString()];
+      } else {
+        selectedValues = [];
+      }
+      selectedValues = selectedValues.map(function (value) {
+        return value.trim();
+      });
+
       $.each(items, function (index, item) {
         var option = new Option(item.prefLabel._value, item.notation);
+        if (selectedValues.includes(item.notation.toString())) {
+          option.selected = true;
+        }
         self.selectElement.append(option);
       });
     },
+
 
     updateCodesField: function () {
       var selectedOptions = this.selectElement.val() || [];
