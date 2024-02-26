9bd1db83dd4b0967fd3cc2300ab5ca4815f40a43
Neda Taherifar <n.taherifar@gmail.com>
2024-02-26 10:22:53 +0800

    Implement paging logic for fetching GCMD fields from API.

--- ckan/src/shared/public/base/js/gcmd-fields-handler-module.js
+++ ckan/src/shared/public/base/js/gcmd-fields-handler-module.js
@@ -2,42 +2,45 @@ this.ckan.module('gcmd-fields-handler-module', function ($, _) {
   return {
     initialize: function () {
       this.selectElement = $('#field-gcmd_keywords');
+      this.selectedValues = null;
+      if (this.options.selectedValues === true) {
+        // Already initialized as an empty set above, so do nothing
+      } else {
+        this.selectedValues = this.options.selectedValues;
+      }
+
+      console.log(this.selectedValues)
+
+
       this.fetchAndPopulateTerms();
     },
-
-    fetchAndPopulateTerms: function () {
+    fetchAndPopulateTerms: function (nextPageUrl) {
       var self = this;
       var proxyUrl = '/api/proxy/fetch_gcmd';
+      var urlToFetch = nextPageUrl || proxyUrl;
+
       $.ajax({
-        url: proxyUrl,
+        url: urlToFetch,
         method: 'GET',
         success: function (data) {
           self.populateDropdown(data.result.items);
-
+          if (data.result.next) {
+            self.fetchAndPopulateTerms(data.result.next);
+          }
         },
         error: function (xhr, status, error) {
           console.error('Error fetching gcmd terms via proxy:', error);
         }
       });
     },
+
     populateDropdown: function (items) {
       var self = this;
-      var selectedValues = this.options.selectedValues;
-      if (typeof selectedValues === 'string') {
-        selectedValues = selectedValues.replace(/^\{|\}$/g, '').split(',');
-      } else if (typeof selectedValues === 'number') {
-        selectedValues = [selectedValues.toString()];
-      } else {
-        selectedValues = [];
-      }
-      selectedValues = selectedValues.map(function (value) {
-        return value.trim();
-      });
-
       $.each(items, function (index, item) {
         var option = new Option(item.prefLabel._value, item._about);
-        if (selectedValues.includes(item._about)) {
+        if (self.selectedValues && self.selectedValues.includes(item._about)) {
           option.selected = true;
+          console.log(item._about)
         }
         self.selectElement.append(option);
       });
