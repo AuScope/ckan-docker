42623bade80a1d5b4192956bb35040114dd03fcf
Neda Taherifar <n.taherifar@gmail.com>
2024-03-18 10:24:50 +0800

    Update GCMD Science Keywords: Populate all terms and add search by page and label content #40

--- ckan/src/shared/public/base/js/gcmd-fields-handler-module.js
+++ ckan/src/shared/public/base/js/gcmd-fields-handler-module.js
@@ -1,50 +1,80 @@
 this.ckan.module('gcmd-fields-handler-module', function ($, _) {
-  return {
-    initialize: function () {
-      this.selectElement = $('#field-gcmd_keywords');
-      this.selectedValues = null;
-      if (this.options.selectedValues === true) {
-        // Already initialized as an empty set above, so do nothing
-      } else {
-        this.selectedValues = this.options.selectedValues;
-      }
+    return {
+        initialize: function () {
+            this.textInputElement = $('#field-gcmd_keywords');
+            this.inputElement = $('#field-gcmd_keywords_code');
 
-      console.log(this.selectedValues)
+            this.initializeSelect2();
+            this.prepopulateSelect2();
 
-
-      this.fetchAndPopulateTerms();
-    },
-    fetchAndPopulateTerms: function (nextPageUrl) {
-      var self = this;
-      var proxyUrl = '/api/proxy/fetch_gcmd';
-      var urlToFetch = nextPageUrl || proxyUrl;
-
-      $.ajax({
-        url: urlToFetch,
-        method: 'GET',
-        success: function (data) {
-          self.populateDropdown(data.result.items);
-          if (data.result.next) {
-            self.fetchAndPopulateTerms(data.result.next);
-          }
         },
-        error: function (xhr, status, error) {
-          console.error('Error fetching gcmd terms via proxy:', error);
-        }
-      });
-    },
 
-    populateDropdown: function (items) {
-      var self = this;
-      $.each(items, function (index, item) {
-        var option = new Option(item.prefLabel._value, item._about);
-        if (self.selectedValues && self.selectedValues.includes(item._about)) {
-          option.selected = true;
-          console.log(item._about)
-        }
-        self.selectElement.append(option);
-      });
-    },
+        initializeSelect2: function () {
 
-  };
+            var self = this;
+            var nextPage = 0;
+            var lastSearchTerm = null;
+
+            this.inputElement.select2({
+                placeholder: "Select GCMD Keywords",
+                delay: 250,
+                minimumInputLength: 3,
+                tags: [],
+                tokenSeparators: [",", " "],
+                multiple: true,
+                cache: true,
+                query: function (query) {
+                    if (lastSearchTerm !== query.term) {
+                        nextPage = 0; 
+                        lastSearchTerm = query.term; 
+                    }
+
+                    var apiUrl = '/api/proxy/fetch_gcmd';
+                    var data = {
+                        page: nextPage, 
+                        keywords: query.term 
+                    };
+
+                    $.ajax({
+                        type: 'GET',
+                        url: apiUrl,
+                        data: data, 
+                        dataType: 'json',
+                        success: function (response) {
+                            nextUrl = response.result.next;
+                            var items = response.result.items.map(function (item) {
+                                return { id: item._about, text: item.prefLabel._value };
+                            });
+                            nextPage = response.result.page + 1;
+                            query.callback({ results: items, more: !!response.result.next });
+                        }
+                    });
+                }
+            }).on("change", function (e) {
+                self.updateDependentFields();
+            });
+        },
+        updateDependentFields: function () {
+            var self = this;
+            var selectedData = self.inputElement.select2('data');
+            var texts = selectedData.map(function (item) { return item.text; });
+            self.textInputElement.val(JSON.stringify(texts));
+        },
+
+        prepopulateSelect2: function () {
+            var self = this;
+            var existingIdsString = this.inputElement.val();
+            var existingIds = existingIdsString ? existingIdsString.split(',') : [];
+
+            var existingTextsString = this.textInputElement.val();
+            var existingTexts = existingTextsString ? JSON.parse(existingTextsString) : [];
+            if (existingIds.length > 0 && existingIds[0] !== "") {
+                var dataForSelect2 = existingIds.map(function (id, index) {
+                    return { id: id, text: existingTexts[index] };
+                });
+                self.inputElement.select2('data', dataForSelect2, true);
+            }
+            // self.inputElement.val(existingIds);
+        },
+    };
 });
