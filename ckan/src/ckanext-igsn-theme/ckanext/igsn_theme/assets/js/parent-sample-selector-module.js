3ef480805c375e86aa9c8fcf0080cf1797810252
Neda Taherifar <n.taherifar@gmail.com>
2024-06-26 11:40:57 +0800

    Fix issue with semantic linking visualization

--- ckan/src/ckanext-igsn-theme/ckanext/igsn_theme/assets/js/parent-sample-selector-module.js
+++ ckan/src/ckanext-igsn-theme/ckanext/igsn_theme/assets/js/parent-sample-selector-module.js
@@ -1,7 +1,6 @@
 this.ckan.module('parent-sample-selector-module', function ($, _) {
   return {
     initialize: function () {
-      this.textInputElement = $('#field-parent-name');
       this.inputElement = $('#field-parent');
       this.initializeSelect2();
       this.prepopulateSelect2();
@@ -10,57 +9,62 @@ this.ckan.module('parent-sample-selector-module', function ($, _) {
     initializeSelect2: function () {
       var self = this;
       this.org_id = this.options.group;
+      console.log(self.org_id);
       this.inputElement.select2({
         placeholder: "Select Parent of Sample",
         minimumInputLength: 0,
         ajax: {
-          url: '/api/3/action/package_search',
+          url: '/api/3/action/organization_show',
+          type: 'GET', 
           dataType: 'json',
           quietMillis: 500,
-          data: function (term, page) {
+          data: function (params) {
+            self.searchTerm = params || '';
             return {
-              q: term,
-              include_private: true,
-              fq: 'owner_org:' + self.org_id // filter query for organization id
+              id: self.org_id,
+              include_datasets: 'true'
             };
           },
-          results: function (data, page) {
+          processResults: function (data) {
             if (!data.success) {
               return { results: [] };
             }
+            var searchTerm = self.searchTerm.toLowerCase();
+            var filteredResults = data.result.packages.filter(function (item) {
+              return item.title.toLowerCase().includes(searchTerm);
+            });
             return {
-              results: data.result.results.map(function (item) {
+              results: filteredResults.map(function (item) {
                 return { id: item.id, text: item.title };
               })
             };
           },
           cache: true
         },
-        formatResult: function (item) { return item.text; },
-        formatSelection: function (item) { return item.text; },
+        templateResult: function (item) { return item.text; },
+        templateSelection: function (item) { return item.text; },
         dropdownCssClass: "bigdrop",
-        escapeMarkup: function (m) { return m; } // we do not want to escape markup since we are displaying html in results
-      }).on("change", function (e) {
-        self.updateDependentFields();
-      });
-    },
-    updateDependentFields: function () {
-      var self = this;
-
-      var selectedData = self.inputElement.select2('data');
-      if (selectedData) {
-        self.textInputElement.val(selectedData.text);
-      }
+        escapeMarkup: function (m) { return m; } 
+      })
     },
 
     prepopulateSelect2: function () {
       var self = this;
       var existingId = this.inputElement.val();
-      var existingText = this.textInputElement.val(); // Assuming you store the selected text in data attribute
-
-      if (existingId && existingText) {
-        var dataForSelect2 = { id: existingId, text: existingText };
-        self.inputElement.select2('data', dataForSelect2, true);
+      if (existingId) {
+        $.ajax({
+          url: '/api/3/action/package_show',
+          data: { id: existingId },
+          type: 'GET',
+          dataType: 'json',
+          success: function (data) {
+            if (data.success && data.result) {
+              var item = data.result;
+              var dataForSelect2 = new Option(item.title, item.id, true, true);
+              self.inputElement.select2('data', dataForSelect2, true);
+            }
+          }
+        });
       }
     }
   };
