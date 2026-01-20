fe4dc5fdb1dbf087c8dffc8cd93637e4fe74ae3f
bmotevalli <b.motevalli@gmail.com>
2026-01-20 11:10:44 +0800

    igsn: integrating rudderstack

--- ckan/src/ckanext-igsn-theme/ckanext/igsn_theme/helpers.py
+++ ckan/src/ckanext-igsn-theme/ckanext/igsn_theme/helpers.py
@@ -5,6 +5,8 @@ from datetime import date
 from ckan.logic import NotFound
 import simplejson as json
 import logging
+import os
+from markupsafe import Markup
 
 def igsn_theme_hello():
     return "Hello, igsn_theme!"
@@ -22,13 +24,13 @@ def is_creating_or_editing_org():
     """Determine if the user is creating or editing an organization."""
     current_path = toolkit.request.path
     if (
-        current_path.startswith('/organization/request_join_collection') or 
+        current_path.startswith('/organization/request_join_collection') or
         current_path.startswith('/organization/request_new_collection') or
         current_path.startswith('/organization/new') or
         current_path.startswith('/organization/edit') or
         current_path.startswith('/organization/members') or
         current_path.startswith('/organization/bulk_process') or
-        current_path == '/organization/' 
+        current_path == '/organization/'
     ):
         return True
     return False
@@ -70,7 +72,7 @@ def get_package(package_id):
         return None
     except toolkit.NotAuthorized:
         return None
-    
+
 
 def get_user_role_in_organization(org_id):
     if not toolkit.c.user:
@@ -108,7 +110,126 @@ def custom_structured_data(dataset_id, profiles=None, _format='jsonld'):
     except ValueError:
         # result was not JSON, return anyway
         return data
-    
+
+
+def rudderstack_script():
+    """
+    Generate RudderStack analytics script tag with configuration from environment variables.
+    Returns the script as safe HTML if RudderStack is enabled, empty string otherwise.
+    """
+    # Check if RudderStack is enabled
+    rudderstack_enabled = toolkit.asbool(os.environ.get('RUDDERSTACK_ENABLED', 'false'))
+
+    if not rudderstack_enabled:
+        return Markup('')
+
+    # Get configuration from environment variables
+    write_key = os.environ.get('RUDDERSTACK_WRITE_KEY', '')
+    data_plane_url = os.environ.get('RUDDERSTACK_DATA_PLANE_URL', '')
+
+    if not write_key or not data_plane_url:
+        logging.warning('RudderStack enabled but WRITE_KEY or DATA_PLANE_URL not configured')
+        return Markup('')
+
+    script = f'''
+<script type="text/javascript">
+(function() {{
+  "use strict";
+  window.RudderSnippetVersion = "3.2.0";
+  var identifier = "rudderanalytics";
+  if (!window[identifier]) {{
+    window[identifier] = [];
+  }}
+  var rudderanalytics = window[identifier];
+  if (Array.isArray(rudderanalytics)) {{
+    if (rudderanalytics.snippetExecuted === true && window.console && console.error) {{
+      console.error("RudderStack JavaScript SDK snippet included more than once.");
+    }} else {{
+      rudderanalytics.snippetExecuted = true;
+      window.rudderAnalyticsBuildType = "legacy";
+      var sdkBaseUrl = "https://cdn.rudderlabs.com";
+      var sdkVersion = "v3";
+      var sdkFileName = "rsa.min.js";
+      var scriptLoadingMode = "async";
+      var methods = [ "setDefaultInstanceKey", "load", "ready", "page", "track", "identify", "alias", "group", "reset", "setAnonymousId", "startSession", "endSession", "consent", "addCustomIntegration" ];
+      for (var i = 0; i < methods.length; i++) {{
+        var method = methods[i];
+        rudderanalytics[method] = function(methodName) {{
+          return function() {{
+            if (Array.isArray(window[identifier])) {{
+              rudderanalytics.push([ methodName ].concat(Array.prototype.slice.call(arguments)));
+            }} else {{
+              var _methodName;
+              (_methodName = window[identifier][methodName]) === null || _methodName === undefined || _methodName.apply(window[identifier], arguments);
+            }}
+          }};
+        }}(method);
+      }}
+      try {{
+        new Function('class Test{{field=()=>{{}};test({{prop=[]}}={{}}){{return prop?(prop?.property??[...prop]):import("");}}}}');
+        window.rudderAnalyticsBuildType = "modern";
+      }} catch (e) {{}}
+      var head = document.head || document.getElementsByTagName("head")[0];
+      var body = document.body || document.getElementsByTagName("body")[0];
+      window.rudderAnalyticsAddScript = function(url, extraAttributeKey, extraAttributeVal) {{
+        var scriptTag = document.createElement("script");
+        scriptTag.src = url;
+        scriptTag.setAttribute("data-loader", "RS_JS_SDK");
+        if (extraAttributeKey && extraAttributeVal) {{
+          scriptTag.setAttribute(extraAttributeKey, extraAttributeVal);
+        }}
+        if (scriptLoadingMode === "async") {{
+          scriptTag.async = true;
+        }} else if (scriptLoadingMode === "defer") {{
+          scriptTag.defer = true;
+        }}
+        if (head) {{
+          head.insertBefore(scriptTag, head.firstChild);
+        }} else {{
+          body.insertBefore(scriptTag, body.firstChild);
+        }}
+      }};
+      window.rudderAnalyticsMount = function() {{
+        (function() {{
+          if (typeof globalThis === "undefined") {{
+            var getGlobal = function getGlobal() {{
+              if (typeof self !== "undefined") {{
+                return self;
+              }}
+              if (typeof window !== "undefined") {{
+                return window;
+              }}
+              return null;
+            }};
+            var global = getGlobal();
+            if (global) {{
+              Object.defineProperty(global, "globalThis", {{
+                value: global,
+                configurable: true
+              }});
+            }}
+          }}
+        }})();
+        window.rudderAnalyticsAddScript("".concat(sdkBaseUrl, "/").concat(sdkVersion, "/").concat(window.rudderAnalyticsBuildType, "/").concat(sdkFileName), "data-rsa-write-key", "{write_key}");
+      }};
+      if (typeof Promise === "undefined" || typeof globalThis === "undefined") {{
+        window.rudderAnalyticsAddScript("https://polyfill-fastly.io/v3/polyfill.min.js?version=3.111.0&features=Symbol%2CPromise&callback=rudderAnalyticsMount");
+      }} else {{
+        window.rudderAnalyticsMount();
+      }}
+      var loadOptions = {{}};
+      rudderanalytics.load("{write_key}", "{data_plane_url}", loadOptions);
+      // Automatically track page view when SDK is ready
+      rudderanalytics.ready(function() {{
+        rudderanalytics.page();
+      }});    }}
+  }}
+}})();
+</script>
+'''
+    return Markup(script)
+
+
 def get_helpers():
     return {
         "igsn_theme_hello": igsn_theme_hello,
@@ -117,8 +238,9 @@ def get_helpers():
         'get_org_list': get_org_list,
         'users_role_in_org': users_role_in_org,
         "get_search_facets" : get_search_facets,
-        'current_date': current_date,        
+        'current_date': current_date,
         "get_package": get_package,
         "get_user_role_in_organization" : get_user_role_in_organization,
-        "custom_structured_data" : custom_structured_data
+        "custom_structured_data" : custom_structured_data,
+        "rudderstack_script": rudderstack_script
     }
