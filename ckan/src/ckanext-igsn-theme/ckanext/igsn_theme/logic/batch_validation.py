05ddb447b552364de5b3814a082b84bcd7f1231d
Vincent Fazio <vjf@users.noreply.github.com>
2026-07-20 13:45:14 +0800

    AUS-4649 Fix for sample repository batch upload creating merged keywords - Allow keywords to be separated by commas - Change separators from semi-colons to commas in the batch upload example file

--- ckan/src/ckanext-igsn-theme/ckanext/igsn_theme/logic/batch_validation.py
+++ ckan/src/ckanext-igsn-theme/ckanext/igsn_theme/logic/batch_validation.py
@@ -614,14 +614,14 @@ def validate_user_keywords(user_keywords: str) -> str:
     :returns: The original string if valid, otherwise a sanitized version.
     """
     # Regular expression pattern for allowed characters
-    pattern = r'^[\w\s.-]+$'
+    pattern = r'^[\w\s,.-]+$'
     
     # Check if the user_keywords match the pattern
     if re.match(pattern, user_keywords):
         return user_keywords
     else:
         # Remove any characters that are not allowed
-        sanitized_keywords = re.sub(r'[^\w\s.-]', ' ', user_keywords)
+        sanitized_keywords = re.sub(r'[^\w\s,.-]', ' ', user_keywords)
         return sanitized_keywords   
 
 def validate_authors(authors_df: pd.DataFrame) -> list[str]:
