6350f240ff7cb608be630204fa2470d5e0875f8f
Vincent Fazio <vjf@users.noreply.github.com>
2026-08-25 17:31:27 +1000

    Simplify redirect parameters in views.py to preserve URL port

--- ckan/src/ckanext-igsn-theme/ckanext/igsn_theme/views.py
+++ ckan/src/ckanext-igsn-theme/ckanext/igsn_theme/views.py
@@ -241,10 +241,7 @@ class BatchUploadView(MethodView):
                 return redirect(url_for(
                     'igsn_theme.batch_job_status_page',
                     job_id=job_id,
-                    group=org_id,
-                    _external=True,
-                    _scheme=request.scheme,
-                    _host=request.host,
+                    group=org_id
                 ))
 
             else:
